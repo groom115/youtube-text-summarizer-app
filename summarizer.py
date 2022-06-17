4244c9fa1df480601224228a924f216a5cd8e536
groom115 <79688511+groom115@users.noreply.github.com>
2022-06-17 21:57:58 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,4 +1,3 @@
-# Gensim Imports
 from gensim.summarization.summarizer import summarize
 
 # Spacy Imports
@@ -30,6 +29,16 @@ def gensim_summarize(text_content, percent):
 
     # Returning NLTK Summarization Output
     return summary
+
+
+
+
+
+
+
+
+
+
 
 
 def spacy_summarize(text_content, percent):
@@ -93,6 +102,15 @@ def spacy_summarize(text_content, percent):
     return summary
 
 
+
+
+
+
+
+
+
+
+
 def nltk_summarize(text_content, percent):
     # Frequency Based Summarization using NLTK
     # Store a tokenized copy of text, using NLTK's recommended word tokenizer
@@ -147,72 +165,5 @@ def nltk_summarize(text_content, percent):
     final_summary = [word for word in summary]
     summary = ' '.join(final_summary)
 
-    # Returning NLTK Summarization Output
-    return summary
-
-
-def sumy_lsa_summarize(text_content, percent):
-    # Latent Semantic Analysis is a unsupervised learning algorithm that can be used for extractive text summarization.
-    # Initializing the parser
-    parser = PlaintextParser.from_string(text_content, Tokenizer("english"))
-    # Initialize the stemmer
-    stemmer = Stemmer('english')
-    # Initializing the summarizer
-    summarizer = LsaSummarizer(stemmer)
-    summarizer.stop_words = get_stop_words('english')
-
-    # Finding number of sentences and applying percentage on it: since sumy requires number of lines
-    sentence_token = sent_tokenize(text_content)
-    select_length = int(len(sentence_token) * (int(percent) / 100))
-
-    # Evaluating and saving the Summary
-    summary = ""
-    for sentence in summarizer(parser.document, sentences_count=select_length):
-        summary += str(sentence)
-    # Returning NLTK Summarization Output
-    return summary
-
-
-def sumy_luhn_summarize(text_content, percent):
-    # A naive approach based on TF-IDF and looking at the “window size” of non-important words between words of high
-    # importance. It also assigns higher weights to sentences occurring near the beginning of a document.
-    # Initializing the parser
-    parser = PlaintextParser.from_string(text_content, Tokenizer("english"))
-    # Initialize the stemmer
-    stemmer = Stemmer('english')
-    # Initializing the summarizer
-    summarizer = LuhnSummarizer(stemmer)
-    summarizer.stop_words = get_stop_words('english')
-
-    # Finding number of sentences and applying percentage on it: since sumy requires number of lines
-    sentence_token = sent_tokenize(text_content)
-    select_length = int(len(sentence_token) * (int(percent) / 100))
-
-    # Evaluating and saving the Summary
-    summary = ""
-    for sentence in summarizer(parser.document, sentences_count=select_length):
-        summary += str(sentence)
-    # Returning NLTK Summarization Output
-    return summary
-
-
-def sumy_text_rank_summarize(text_content, percent):
-    # TextRank is an unsupervised text summarization technique that uses the intuition behind the PageRank algorithm.
-    # Initializing the parser
-    parser = PlaintextParser.from_string(text_content, Tokenizer("english"))
-    # Initialize the stemmer
-    stemmer = Stemmer('english')
-    # Initializing the summarizer
-    summarizer = TextRankSummarizer(stemmer)
-    summarizer.stop_words = get_stop_words('english')
-
-    # Finding number of sentences and applying percentage on it: since sumy requires number of lines
-    sentence_token = sent_tokenize(text_content)
-    select_length = int(len(sentence_token) * (int(percent) / 100))
-
-    # Evaluating and saving the Summary
-    summary = ""
-    for sentence in summarizer(parser.document, sentences_count=select_length):
-        summary += str(sentence)
     # Returning NLTK Summarization Output
     return summary
